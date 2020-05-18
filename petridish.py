ecee1f9bbd42f06e34e48512f33708ca09f355ab
Aditya Rawal <aditya.rawal@uber.com>
2020-05-18 13:28:46 -0700

    Adding LICENSE

--- petridish.py
+++ petridish.py
@@ -1,3 +1,13 @@
+# Copyright (c) 2020 Uber Technologies, Inc.
+#
+# Licensed under the Uber Non-Commercial License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at the root directory of this project.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 """A multi-layer perceptron for classification of MNIST handwritten digits."""
 from __future__ import absolute_import, division
 from __future__ import print_function
--- plot_data.py
+++ plot_data.py
@@ -1,3 +1,12 @@
+# Copyright (c) 2020 Uber Technologies, Inc.
+#
+# Licensed under the Uber Non-Commercial License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at the root directory of this project.
+#
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 train_sorted_c_r_list = [
 (0.5756565656565656, 0.6966641782509908), 
 (0.8786868686868686, 0.6997672109215385), 
